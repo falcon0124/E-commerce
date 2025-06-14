5191e1455f1369e44adec38cbce97b4265e84652
sangam saurav <sangamsh04@gmail.com>
2025-06-15 00:57:17 +0530

    finished cart functionality

--- controllers/cart.js
+++ controllers/cart.js
@@ -6,9 +6,9 @@ async function addToCart(req, res) {
         const userId = req.user._id;
 
         const newItem = {
-                user: userId,
-                items: [{ product: productId, quantity: Number(quantity) }],
-            }
+            user: userId,
+            items: [{ product: productId, quantity: Number(quantity) }],
+        }
         const userCart = await Cart.findOne({ user: userId });
 
         if (!userCart) {
@@ -17,8 +17,8 @@ async function addToCart(req, res) {
         } else {
             const existingItem = userCart.items.find(item => item.product.toString() === productId);
             if (existingItem) {
-               existingItem.quantity += Number(quantity);
-            }else{
+                existingItem.quantity += Number(quantity);
+            } else {
                 userCart.items.push({ product: productId, quantity: Number(quantity) });
             }
             await userCart.save()
@@ -30,4 +30,59 @@ async function addToCart(req, res) {
     }
 }
 
-module.exports = addToCart;
+async function viewCart(req, res) {
+    const userId = req.user._id;
+    try {
+        const userCart = await Cart.findOne({ user: userId })
+            .populate('items.product');
+        
+        if(!userCart || userCart.items.length === 0){
+            return res.status(200).json({ message: 'Cart empty'});
+        }
+        return res.status(200).json({ message: `${userCart.items.length} found in cart`, cart: userCart });
+    } catch(err){
+        return res.status(400).json({ message: 'Error fetching cart' });
+    }
+}
+
+async function deleteItem(req, res) {
+    const {productId} = req.params;
+    const userId = req.user._id;
+
+    try{
+        const userCart = await Cart.findOne({user: userId});
+        
+        const index = userCart.items.findIndex(item => item.product.toString() === productId);
+
+        if(index === -1){
+            return res.status(200).json({message: 'item not in cart'});
+        }
+
+        userCart.items.splice(index, 1);
+        await userCart.save()
+
+        return res.status(200).json({message: 'Item removed', cart : userCart});
+    }catch(err){
+        return res.status(400).json({message: 'An error occured'});
+    }
+}
+
+async function clearCart(req, res) {
+    const userId = req.user._id;
+    try{
+        const userCart = await Cart.findOne({ user: userId });
+    
+        if(!userCart){
+            return res.status(200).json({message: 'cart does not exist'});
+        } 
+        
+        userCart.items = [];
+        await userCart.save();
+
+        return res.status(200).json({message: 'Cart emptied', cart: userCart});
+    }catch(err){
+        
+        return res.status(400).json({message: 'an error occured'});
+    }
+}
+module.exports = {addToCart, viewCart, deleteItem, clearCart};
--- routes/cart.js
+++ routes/cart.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const verifyToken = require('../middlewares/protectRoute');
-const addToCart = require('../controllers/cart');
+const {addToCart, viewCart, deleteItem, clearCart} = require('../controllers/cart');
 
 const router = express.Router();
 
 router.post('/add-items', verifyToken, addToCart);
+router.get('/view', verifyToken, viewCart);
+router.delete('/remove/:productId', verifyToken, deleteItem);
+
+router.delete('/clear', verifyToken, clearCart);
 
 module.exports = router;
