13c2c2e3a31793a7ca859ec88a8b2309171ac779
sangam saurav <sangamsh04@gmail.com>
2025-07-09 20:36:21 +0530

    fixing deploymen8

--- frontend-vite/src/App.jsx
+++ frontend-vite/src/App.jsx
@@ -5,7 +5,6 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import Cart from './pages/Cart';
 import Profile from './pages/Profile';
-import Products from './pages/Products';
 import ProductDetails from './pages/ProductDetails';
 import PlaceOrder from './pages/PlaceOrder';
 import AddItem from './pages/AddItem';
@@ -18,7 +17,7 @@ function App() {
         <Route index element={<Home />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
-        <Route path="cart" element={<Cart />} />
+        <Route path="Cart" element={<Cart />} />
         <Route path="Profile" element={<Profile />} />
         <Route path="AddItem" element={<AddItem />} />
         <Route path="placeOrder" element={<PlaceOrder />} />
