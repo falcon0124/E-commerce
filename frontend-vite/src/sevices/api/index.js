d1f4a180892076857204209773c44d7669d48f21
sangam saurav <sangamsh04@gmail.com>
2025-07-09 20:02:38 +0530

    fixing deployment5

--- frontend-vite/src/sevices/api/index.js
+++ frontend-vite/src/sevices/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://e-commerce-utma.onrender.com', 
+  baseURL: 'https://e-commerce-utma.onrender.com/api', 
 });
 
 axiosInstance.interceptors.request.use((config) => {
